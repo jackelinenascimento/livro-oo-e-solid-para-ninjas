bbbfcf02d43581de139638862de35f77e0b23c15
Jackie Nascimento <jackeline_nascimento@hotmail.com>
2026-02-07 10:00:47 -0300

    Initial configuration

---
+++ app/src/main/kotlin/com/clubedeleitura/orientacaoobjetossolid/OrientacaoobjetossolidApplication.kt
@@ -0,0 +1,11 @@
+package com.clubedeleitura.orientacaoobjetossolid
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrientacaoobjetossolidApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrientacaoobjetossolidApplication>(*args)
+}
---
+++ app/src/test/kotlin/com/clubedeleitura/orientacaoobjetossolid/OrientacaoobjetossolidApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.clubedeleitura.orientacaoobjetossolid
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrientacaoobjetossolidApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
